026b5353336a039f83a206c9f87a26972c596b17
Marco Antonio Tupa Arela <matupaarela@outlook.com>
2020-09-06 23:31:40 -0500

    Falta Integrar Login e inserciones masivas

--- Presentation/Forms/FormEmployee.Designer.cs
+++ Presentation/Forms/FormEmployee.Designer.cs
@@ -45,6 +45,7 @@
             this.DTPBirthay = new System.Windows.Forms.DateTimePicker();
             this.BtnSearch = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGVEmployee)).BeginInit();
             this.PnlControls.SuspendLayout();
             this.SuspendLayout();
@@ -206,11 +207,21 @@
             this.label5.Text = "Inserciones Masivas: https://www.youtube.com/watch?v=fne9TMDlLN4&list=PLqjdFmR_Hd" +
     "QTHZN6FYll8PCQy37QZ2-Xq&index=3&ab_channel=RJCodeAdvance";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Falta Integrar Login";
+            // 
             // FormEmployee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(709, 330);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.BtnSearch);
             this.Controls.Add(this.BtnNew);
@@ -249,5 +260,6 @@
         private System.Windows.Forms.Button BtnNew;
         private System.Windows.Forms.Button BtnSearch;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
